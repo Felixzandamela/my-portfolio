d44a36f032a7f833d72fe3a3f91b7df3526b8a1a
Felixzandamela <81531283+Felixzandamela@users.noreply.github.com>
2023-04-10 14:17:36 +0200

    update src/pages/Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,7 +12,7 @@ const Header = ()=>{
   return (
     <header className="header">
       <div className="header-container">
-        <img src="./assets/header.jpg" className="image" />
+        <img src="./assets/header.jpg" className="image" alt="header"/>
       <div>
         <h3 className="heading">Hi, I'm Félix Zamdamela</h3>
         <p className="description"> Welcome to my portfolio, the place where a piece of me is reflected in every detail. I am inspired by you and
