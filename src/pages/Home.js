f0fd91d88ed163787613f2c115172f48a6bdc02a
Felixzandamela <81531283+Felixzandamela@users.noreply.github.com>
2023-04-10 13:23:34 +0200

    update src/pages/Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,9 +15,7 @@ const Header = ()=>{
         <img src="/header.jpg" className="image" />
       <div>
         <h3 className="heading">Hi, I'm Félix Zamdamela</h3>
-        <p className="description">Welcome to my portfolio, the place where a
-        piece of me is reflected in every detail.
-        I am inspired by you and
+        <p className="description"> Welcome to my portfolio, the place where a piece of me is reflected in every detail. I am inspired by you and
         everyone who is interested in what I do. </p>
         <a href="https://wa.me/message/7VVAOYC3TSBGB1" target="_blank">
           <button className="header-button">Contact-me</button>
