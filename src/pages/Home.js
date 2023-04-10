07eb21368b37e6253f5302a4785f63dbaea84cc4
Felixzandamela <81531283+Felixzandamela@users.noreply.github.com>
2023-04-10 16:11:01 +0200

    update src/pages/Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -25,4 +25,40 @@ const Header = () =>{
   ) 
 }
 
+ // get years function 
+const activeYears = (date) => {
+ let time = (new Date()).getTime() - date.getTime(); 
+  let year = Math.floor(time / 1000/60/60/24/30/12); 
+  return year; 
+}
+
+// Skills  component
+const Skills = ()=>{
+ let yearsOffWork = activeYears(new Date("Oct, 12 2020"));
+
+  return(
+    <section className="sec">
+      <div className="sec-container">
+      <div className="sec-header">
+        <h3 className="title">My <span>experience</span> and knowledge</h3>
+        <p className="featuresubheader">
+         I'm a fullstack Web  applicatons developer based in Mozambique with over {yearsOffWork} years of experience in:
+         </p>
+      </div>
+      <div class="flex-wrap-center">
+       {dataSkills.map(skill => (
+          <SkillCard
+            skillName={skill.name}
+            key={skill.name}
+            skillValue={skill.value}
+            skillColor={skill.color}
+          />
+       ))}
+      </div>
+      </div>
+    </section>
+  )
+}
+
+
 export default Home;
